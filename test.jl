fd4f9eebe5420dff71ce0da12c50edd96a42c371
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-12 17:53:20 +0100

    Implemented Critters, automaton, and animation
    * automata now "playable" in console (UnicodePlots)
    * Critters rule works
    * rough basis for a module is done

--- test.jl
+++ test.jl
@@ -1,57 +1,81 @@
-test_string = "halo i bims 1 idiot lol"
-char_rep = collect(test_string)
-int_rep = Int8.(char_rep)
-bit_rep = bitstring.(int_rep)
+using UnicodePlots, ColorSchemes, Random
 
-m = [[parse(Bool, i) for i in row] for row in bit_rep]
+# Create initial state (later: clear text)
+ct = "halo i bims 1 idiot lol"
+ct_char = collect(ct)
+ct_int = Int8.(ct_char)
+ct_bitstr = bitstring.(ct_int)
+ct_m = [[parse(Bool, i) for i in row] for row in ct_bitstr]
+if mod(length(ct_m), 2) != 0
+    pushfirst!(ct_m, zeros(Bool, length(ct_m[1])))
+end
+mat = hcat(ct_m...)'
 
-if mod(length(m), 2) != 0
-    pushfirst!(m, zeros(Bool, length(m[1])))
+
+# Create cellular automaton
+mutable struct Automaton
+    states::Array{Any}
 end
 
-# MARGOLUS NEIGHBORHOOD SCHEME
-
-function start_at(t)
-    return mod(t, 2) == 0 ? (1, 1) : (2, 2)
+function run!(automaton::Automaton, steps::Int)
+    for step in 0:steps
+        curr_state = deepcopy(automaton.states[end])
+        start = mod(step, 2) == 0 ? 1 : 2
+        next_state = zeros(Bool, size(curr_state))
+        for i in start:2:size(curr_state)[1]
+            for j in start:2:size(curr_state)[2]
+                pos1 = [i, j]
+                pos2 = [i, j+1 <= size(curr_state)[2] ? j+1 : 1]
+                pos3 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j]
+                pos4 = [i+1 <= size(curr_state)[1] ? i+1 : 1, j+1 <= size(curr_state)[2] ? j+1 : 1]
+                margolus = [
+                    curr_state[pos1...] curr_state[pos2...];
+                    curr_state[pos3...] curr_state[pos4...]
+                ]
+                block_sum = sum(margolus)
+                if block_sum == 2
+                    next_state[pos1...] = curr_state[pos1...]
+                    next_state[pos2...] = curr_state[pos2...]
+                    next_state[pos3...] = curr_state[pos3...]
+                    next_state[pos4...] = curr_state[pos4...]
+                elseif block_sum in [0, 1, 4]
+                    next_state[pos1...] = !curr_state[pos1...]
+                    next_state[pos2...] = !curr_state[pos2...]
+                    next_state[pos3...] = !curr_state[pos3...]
+                    next_state[pos4...] = !curr_state[pos4...]
+                else
+                    next_state[pos1...] = !curr_state[pos4...]
+                    next_state[pos2...] = !curr_state[pos3...]
+                    next_state[pos3...] = !curr_state[pos2...]
+                    next_state[pos4...] = !curr_state[pos1...]
+                end
+            end
+        end
+        push!(automaton.states, deepcopy(next_state))
+    end
+    return automaton
 end
 
-t = 1
-start = start_at(t)
-
-i = 1
-
-for i in start[1]:2:length(m)
-    for j in start[2]:2:length(m[1])
-        margolus = [
-            [
-                m[i][j],
-                m[i][j+1 <= length(m[1]) ? j+1 : 1]
-            ],
-            [
-                m[i+1 <= length(m) ? i+1 : 1][j],
-                m[i+1 <= length(m) ? i+1 : 1][j+1 <= length(m[1]) ? j+1 : 1]
-            ]
-        ]
-        println(margolus)
+function animate(automaton::Automaton, width::Int=20)
+    for frame in automaton.states
+        Base.run(`clear`)
+        print(heatmap(frame, colorbar = false, colormap = [(0, 0, 0), (255, 255, 255)], width = width))
+        sleep(0.2)
     end
 end
 
+# Example 1: clear text
+Base.run(`clear`)
+println("Run automaton on \"clear text\" from above.")
+sleep(1)
+automaton = Automaton([mat])
+run!(automaton, 100)
+animate(automaton, 8)
 
-# TODO
-#
-# * [ ] implement Critters rule
-# * [ ] implement Tron rule
-# * [ ] encode / decode
-
-
-
-# BLOCK CELLULAR AUTOMATA COMPONENTS (WIKIPEDIA)
-#
-# * a regular lattice of cells
-# * a finite set of the states that each cell may be in
-# * a partition of the cells into a uniform tessellation in which each tile of the partition has the same size an shape (NOTE: for example Margolus neighborhood)
-# * a rule for shifting the partition after each time step
-# * a transition rule, a function that takes as input an assignment of states for the cells in a single tile and produces as output another assignment of states for the same cells
-
-
-
+# Example 2: random
+Base.run(`clear`)
+println("Run automaton on random matrix.")
+sleep(1)
+automaton = Automaton([Random.rand(Bool, 50, 50)])
+run!(automaton, 100)
+animate(automaton, 50)
