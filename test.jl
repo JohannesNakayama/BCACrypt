4d82a4c6c605d84100150dce430c46d6a52c6616
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-12 13:50:17 +0100

    Wonky implementation of Margolus neighborhood
    * works, but kind of strange

--- test.jl
+++ test.jl
@@ -1,22 +1,43 @@
 test_string = "halo i bims 1 idiot lol"
 char_rep = collect(test_string)
 int_rep = Int8.(char_rep)
-bit_rep = bitstring.(int_rep)
+bit_string_rep = bitstring.(int_rep)
 
 
 # MARGOLUS NEIGHBORHOOD SCHEME
-#
-# if t mod 2 == 0
-#   start = [1, 1]
-# else
-#   start = [2, 2]
-# end
-#
+
+function start_at(t)
+    return mod(t, 2) == 0 ? (1, 1) : (2, 2)
+end
+
+m = [[parse(Bool, i) for i in row] for row in bit_rep]
+t = 2
+start = start_at(t)
+
+i = 1
+
+m
+for j in start[2]:2:length(m[1])
+    margolus = [
+        [
+            m[i][j <= length(m[1]) ? j : 1],
+            m[i][j+1 <= length(m[1]) ? j+1 : 1]
+        ],
+        [
+            m[i+1][j <= length(m[1]) ? j : 1],
+            m[i+1][j+1 <= length(m[1]) ? j+1 : 1]
+        ]
+    ]
+    println(margolus)
+end
+
+
 # for i in start[1]:nrows(mat)
 #   for j in start[2]:8  # if 8-bit strings are used
 #     # Margolus neighborhood given by:
 #     mat[i,   j  ], mat[i  , j+1]
 #     mat[i+1, j  ], mat[i+1, j+1]
+#       --> broadcast -> mod 8
 #     -> apply rule
 #     # end of row flips over to beginning if start = [2, 2] (modulo)
 #   end
